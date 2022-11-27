e0bd0714466e7cee536c08447d7f2d53c7c951ac
marodseg <manucazorla9@gmail.com>
2022-11-27 17:26:38 +0100

    Create documentation of the API

--- main.py
+++ main.py
@@ -4,7 +4,109 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes import organizations, auth, animals, users, petitions, filters
 from fastapi import FastAPI
 
-app = FastAPI()
+description = """
+**Confianza Animal API** helps you do to manage a database of animals, organizations and users.
+
+This API is part of the **Confianza Animal project**:
+
+* A **web application** that helps an organization to manage a database of animals 
+* An **android application** that helps users to find a pet to adopt.
+
+## Organizations
+
+You can:
+
+* **Create** an organization.
+* **Login** in the the web application.
+* **Get** your own information.
+* **Update** your organization.
+* **Disable/Enable** your organization.
+* **Get** all the organizations.
+* **Get** a specific organization by name.
+* **Upload** a photo of your organization.
+* **Upload/Update** an animal **(dog or cat)** in your organization.
+* **Get** all the animals of your organization.
+* **Update** an animal of your organization.
+
+## Authentication (for users and organizations)
+
+You can:
+
+* **Reset** your password.
+
+## Users
+
+You can:
+
+* **Create** a user.
+* **Login** in the the android application.
+* **Get** your own information.
+* **Update** your user.
+* **Disable/Enable** your user.
+* **Update** your user's photo.
+* **Get** all the animals requested by your user.
+
+## Animals (managed by organizations)
+
+You can:
+
+* **Get** all the animals.
+* **Get** a specific animal **(dog/cat)** by id.
+* **Get** all the animals with filters:
+    * **Size**.
+        * **Mini**.
+        * **Small**.
+        * **Medium**.
+        * **Big**.
+        * **Very big**.
+    * **Age:** with the parameter ***greater_or_equal*** allows you to choose if the age of the animal must be ≥ or ≤ than the number specified in the age field.
+    * **Gender**.
+        * **Male**.
+        * **Female**.
+    * **Raze (Chihuahua, bulldog, dalmata, ..., or other)**.
+    * **Activity**.
+        * **Low**.
+        * **Medium**.
+        * **High**.
+    * **Urgency:** specify if the adoption of the animal is urgent or not.
+* **Upload/Delete photos of specific animals by id**.
+
+## Petitions (managed by users and organizations)
+
+You can:
+
+* **Create** a petition for a specific animal **(dog/cat)** by id.
+* **Get** all the petitions of your organization.
+* **Get** all the petitions of your user.
+* **Delete** a petition of your organization by id.
+* **Delete** a petition of your user by id.
+
+## Filters
+
+You can:
+
+* **Get** all the filters.
+* **Get** the filters by province.
+* **Get** the filters by gender.
+* **Get** the filters by size.
+* **Get** the filters by dog raze.
+* **Get** the filters by cat raze.
+"""
+
+app = FastAPI(
+    title="Confianza Animal",
+    description=description,
+    version="0.0.1",
+    contact={
+        "name": "Manuel Ángel Rodríguez Segura",
+        "url": "http://linkedin.com/in/marodseg",
+        "email": "manuelangelrodriguezsegura@gmail.com",
+    },
+    license_info={
+        "name": "License Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0",
+    },
+)
 
 origins = [
     "http://localhost.tiangolo.com",
