24658d3078a2706df43a9c529309e4f40e09d5a3
marodseg <manucazorla9@gmail.com>
2022-12-10 13:27:42 +0100

    Configure env variables in Github Actions

--- app/config/firebase_config.py
+++ app/config/firebase_config.py
@@ -5,23 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 firebase_config = {
-    "apiKey": os.getenv("FIREBASE_API_KEY"),
-    "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
-    "projectId": os.getenv("FIREBASE_PROJECT_ID"),
-    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
-    "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
-    "appId": os.getenv("FIREBASE_APP_ID"),
-    "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID"),
-    "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
+    "apiKey": os.environ["FIREBASE_API_KEY"],
+    "authDomain": os.environ["FIREBASE_AUTH_DOMAIN"],
+    "projectId": os.environ["FIREBASE_PROJECT_ID"],
+    "storageBucket": os.environ["FIREBASE_STORAGE_BUCKET"],
+    "messagingSenderId": os.environ["FIREBASE_MESSAGING_SENDER_ID"],
+    "appId": os.environ["FIREBASE_APP_ID"],
+    "measurementId": os.environ["FIREBASE_MEASUREMENT_ID"],
+    "databaseURL": os.environ["FIREBASE_DATABASE_URL"],
 }
 
 test_firebase_config = {
-    "apiKey": os.getenv("TEST_FIREBASE_API_KEY"),
-    "authDomain": os.getenv("TEST_FIREBASE_AUTH_DOMAIN"),
-    "projectId": os.getenv("TEST_FIREBASE_PROJECT_ID"),
-    "storageBucket": os.getenv("TEST_FIREBASE_STORAGE_BUCKET"),
-    "messagingSenderId": os.getenv("TEST_FIREBASE_MESSAGING_SENDER_ID"),
-    "appId": os.getenv("TEST_FIREBASE_APP_ID"),
-    "measurementId": os.getenv("TEST_FIREBASE_MEASUREMENT_ID"),
-    "databaseURL": os.getenv("TEST_FIREBASE_DATABASE_URL"),
+    "apiKey": os.environ["TEST_FIREBASE_API_KEY"],
+    "authDomain": os.environ["TEST_FIREBASE_AUTH_DOMAIN"],
+    "projectId": os.environ["TEST_FIREBASE_PROJECT_ID"],
+    "storageBucket": os.environ["TEST_FIREBASE_STORAGE_BUCKET"],
+    "messagingSenderId": os.environ["TEST_FIREBASE_MESSAGING_SENDER_ID"],
+    "appId": os.environ["TEST_FIREBASE_APP_ID"],
+    "measurementId": os.environ["TEST_FIREBASE_MEASUREMENT_ID"],
+    "databaseURL": os.environ["TEST_FIREBASE_DATABASE_URL"],
 }
