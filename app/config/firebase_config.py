8e55a08d77de46436d426d8b7360e96173b268c0
marodseg <manucazorla9@gmail.com>
2022-12-10 13:44:45 +0100

    Configure env variables in Github Actions

--- app/config/firebase_config.py
+++ app/config/firebase_config.py
@@ -1,8 +1,8 @@
 import os
 
-# from dotenv import load_dotenv
-#
-# load_dotenv()
+from dotenv import load_dotenv
+
+load_dotenv()
 
 firebase_config = {
     "apiKey": os.environ.get("FIREBASE_API_KEY"),
